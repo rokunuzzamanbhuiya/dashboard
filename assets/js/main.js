4bf1eafb13c7f1bcccddacd55ae3ad26f4c10743
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2024-07-08 15:55:23 +0600

    preloader added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Theme Name: Dashboard - Welcome To Dashboard
+   Author: Rokunuzzaman Bhuiya
+   Version: 1.0.0
+   File Description: Main JS file of the template
+*/
+
+"use strict";
+/*=============================================
+	=    		Preloader			      =
+=============================================*/
+window.addEventListener("load", function () {
+  document.querySelector("body").classList.add("loaded");
+});
